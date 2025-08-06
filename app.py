e5596a4ec5abbb0764a874cbe79cd93df8361996
Bhavya <bhavyahshree@gmail.com>
2025-08-05 21:14:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -293,7 +293,7 @@ class StreamlitApp:
                         net.show(f"sbml_network_{model_id}.html")
 
                         HtmlFile = open(f"sbml_network_{model_id}.html", "r", encoding="utf-8")
-                        st.components.v1.html(HtmlFile.read(), height=600)
+                        st.components.v1.html(HtmlFile.read(), height=600, width=1200)
                         
             GROQ_API_KEY = st.text_input("Enter a GROQ API key (which is free to make!):", key = "api_keys")
             url = "https://console.groq.com/keys"
